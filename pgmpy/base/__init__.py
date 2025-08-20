db81a760a6d52083090edd29b51ddd44750628e2
daehyun99 <daehyun99@kakao.com>
2025-08-20 15:47:03 +0900

    Fix: Reorder imports to resolve TypeError (#2307)
    - The isort-enforced import order breaks the tests with a `TypeError`.
    - Reverting to the previous order fixes it.

--- pgmpy/base/__init__.py
+++ pgmpy/base/__init__.py
@@ -1,5 +1,6 @@
-from .ADMG import ADMG
-from .DAG import DAG, PDAG
 from .UndirectedGraph import UndirectedGraph
+
+from .DAG import DAG, PDAG  # isort: skip
+from .ADMG import ADMG  # isort: skip
 
 __all__ = ["UndirectedGraph", "DAG", "PDAG", "ADMG"]
