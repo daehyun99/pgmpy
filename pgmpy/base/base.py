1023d8321901662b6bbcf0010a3998655cecfe77
daehyun99 <daehyun99@kakao.com>
2025-10-24 16:54:24 +0900

    Refactor: make a frame of `CoreGraph` class (#2383)
    - Since `CoreGraphABC` is an internal class, I rename it to `_CoreGraphABC`.
    - Added `exposures` and `outcomes` constants to `__init__()`.
    - Modified the abstract class. Instead of implementing `is_directed()` and `is_multigraph()` methods in subclasses, we now define `IS_DIRECTED`, `IS_MULTIGRAPH`, and `ALLOWED_EDGES`.

--- pgmpy/base/base.py
+++ pgmpy/base/base.py
@@ -1,3 +1,4 @@
+import inspect
 from abc import ABC, abstractmethod
 from typing import Hashable, Iterable, Optional
 
@@ -6,45 +7,118 @@ import networkx as nx
 from pgmpy.base._mixin_roles import _GraphRolesMixin
 
 
-class CoreGraphABC(ABC):
+class _CoreGraphABC(ABC):
     """
-    An abstract class that all graphs must implement.
-    It must be the first class inherited by all graphs.
+    An abstract class.
+
+    Inheriting from `CoreGraph` automatically includes inheritance from `_CoreGraphABC`.
     """
+
+    # Constants that the graph class must implement.
+    ALLOWED_EDGES = None
+    IS_DIRECTED = None
+    IS_MULTIGRAPH = None
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+
+        if inspect.isabstract(cls):
+            return
+
+        if cls.IS_DIRECTED is None:
+            raise TypeError("IS_DIRECTED must be defined.")
+
+        if cls.IS_MULTIGRAPH is None:
+            raise TypeError("IS_MULTIGRAPH must be defined.")
+
+        if cls.ALLOWED_EDGES is None:
+            raise TypeError("ALLOWED_EDGES must be defined.")
 
     @abstractmethod
-    def is_directed(self):
-        """Returns True if graph is directed, False otherwise."""
+    def _graph_type_marker(self):
+        """
+        This is a pure virtual method to prevent this class (and CoreGraph) from being instantiated directly.
+        Concrete classes like DAG, PDAG, etc., must implement it.
+        """
         pass
 
-    @abstractmethod
-    def is_multigraph(self):
-        """Returns True if graph is a multigraph, False otherwise."""
-        pass
 
-
-class CoreGraph(CoreGraphABC, nx.MultiGraph, _GraphRolesMixin):
+class CoreGraph(_CoreGraphABC, nx.MultiGraph, _GraphRolesMixin):
     """
     Base graph class.
 
     Parameters
     ----------
+    ebunch : input graph (optional, default: None)
+    latents : set of nodes (default: empty set)
+    exposures : set of nodes (default: empty set)
+    outcomes : set of nodes (default: empty set)
+    roles : dict, optional (default: None)
 
     Examples
     --------
+    Create an empty CoreGraph with no nodes and no edges.
+
     >>> from pgmpy.base import CoreGraph
     >>> G = CoreGraph()
+
+    Edges and vertices can be passed to the constructor as an edge list.
+
+    >>> [Example_Code]
+
+    **Nodes:**
+
+    Add one node,
+
+    >>> [Example_Code]
+
+    Add a list of nodes,
+
+    >>> [Example_Code]
+
+    **Edges:**
+
+    G can also be grown by adding edges.
+
+    Add one edge,
+
+    >>> [Example_Code]
+
+    Add a list of edges,
+
+    >>> [Example_Code]
+
+    Remove one edge,
+
+    >>> [Example_Code]
+
+    Remove a list of edges,
+
+    >>> [Example_Code]
+
+    **Exposures, Outcomes, and Latents:**
+        [Explain] about difference between Roles and Exposures, Outcomes, and Latents.
+
+    >>> [Example_Code]
+
+    **Roles:**
+
+    >>> [Example_Code]
 
     """
 
     def __init__(
         self,
         ebunch: Optional[Iterable[tuple[Hashable, Hashable]]] = None,
+        exposures: set[Hashable] = set(),
+        outcomes: set[Hashable] = set(),
         latents: set[Hashable] = set(),
         roles=None,
     ):
         super().__init__(ebunch)
 
+        self.exposures = set(exposures)
+        self.outcomes = set(outcomes)
         self.latents = set(latents)
 
         if roles is None:
@@ -58,13 +132,19 @@ class CoreGraph(CoreGraphABC, nx.MultiGraph, _GraphRolesMixin):
 
     def is_directed(self):
         """Returns True if graph is directed, False otherwise."""
-        return False
+        return self.IS_DIRECTED
 
     def is_multigraph(self):
         """Returns True if graph is a multigraph, False otherwise."""
-        return True
+        return self.IS_MULTIGRAPH
 
-    def add_edge(self):
+    def add_edge(
+        self,
+        u: Hashable,  # Source node
+        v: Hashable,  # Destination node
+        type: tuple,  # (type at u, type at v) - type can be one of `-`, `>`, `o`
+        **kwargs,
+    ):
         """
         [Explain].
 
@@ -77,7 +157,7 @@ class CoreGraph(CoreGraphABC, nx.MultiGraph, _GraphRolesMixin):
         >>> G = CoreGraph()
 
         """
-        ...
+        super().add_edge(u, v)
 
     def add_edges_from(self):
         """
