03ee99ec00ae6da0fe8d1c001d710c96a52ee51a
김대현 (Kim DaeHyun) <daehyun99@kakao.com>
2026-06-10 00:22:02 +0900

    Feat: Add `BaseParameter`

---
+++ pgmpy/parameterization/_base.py
@@ -0,0 +1,18 @@
+from skbase.base import BaseEstimator as _BaseEstimator
+
+
+class BaseParameter(_BaseEstimator):
+    _config = {}
+
+    _tags = {
+        "variable_type": "discrete",
+        "produces_factor": False,
+        "is_linear_gaussian": False,
+        "supports_fit_joint": False,
+        "python_dependencies": [],
+    }
+
+    @property
+    def name(self):
+        """Return the name of the object or estimator."""
+        return self.__class__.__name__
