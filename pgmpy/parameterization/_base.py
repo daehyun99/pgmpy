5898e835944e8a3fb2feea042e3853359d508c19
김대현 (Kim DaeHyun) <daehyun99@kakao.com>
2026-06-10 20:45:09 +0900

    Feat: Apply Review
    - Remove `name()`
    - Add `fit()`, `predict_proba()`
    - Add test code

--- pgmpy/parameterization/_base.py
+++ pgmpy/parameterization/_base.py
@@ -5,14 +5,58 @@ class BaseParameter(_BaseEstimator):
     """Base class for all parameter classes in pgmpy."""
 
     _tags = {
-        "variable_type": "discrete",
-        "produces_factor": False,
-        "is_linear_gaussian": False,
-        "supports_fit_joint": False,
-        "python_dependencies": (),
+        "variable_type": str,
+        "produces_factor": bool,
+        "is_linear_gaussian": bool,
+        "supports_fit_joint": bool,
+        "python_dependencies": tuple,
     }
 
-    @property
-    def name(self):
-        """Return the name of the object or estimator."""
-        return self.__class__.__name__
+    def fit(self, X, y):
+        """API docs"""
+        return self._fit(X, y)
+
+    def _fit(self, X, y):
+        """Fit parameter to training data.
+
+        Writes to self:
+            Sets fitted model attributes ending in "_".
+
+        Parameters
+        ----------
+        X : pandas DataFrame
+            feature instances to fit regressor to
+        y : pandas DataFrame, must be same length as X
+            labels to fit regressor to
+
+        Returns
+        -------
+        self : reference to self
+        """
+        raise NotImplementedError
+
+    def predict_proba(self, X):
+        """API docs"""
+        y_pred = self._predict_proba(X)
+        return y_pred
+
+    def _predict_proba(self, X):
+        """Predict distribution over labels for data from features.
+
+        State required:
+            Requires state to be "fitted".
+
+        Accesses in self:
+            Fitted model attributes ending in "_"
+
+        Parameters
+        ----------
+        X : pandas DataFrame, must have same columns as X in `fit`
+            data to predict labels for
+
+        Returns
+        -------
+        y : skpro BaseDistribution, same length as `X`
+            labels predicted for `X`
+        """
+        raise NotImplementedError
--- pgmpy/tests/test_parameterization/test_base.py
+++ pgmpy/tests/test_parameterization/test_base.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import pytest
+from sklearn.datasets import make_moons
+
 from pgmpy.parameterization._base import BaseParameter
 
 
@@ -7,9 +11,26 @@ class TestBaseParameter:
     def test_base_parameter_default(self):
         parameter = BaseParameter()
 
-        assert parameter.name == "BaseParameter"
-        assert parameter.get_class_tag("variable_type") == "discrete"
-        assert parameter.get_class_tag("produces_factor") is False
-        assert parameter.get_class_tag("is_linear_gaussian") is False
-        assert parameter.get_class_tag("supports_fit_joint") is False
-        assert parameter.get_class_tag("python_dependencies") == ()
+        assert parameter.__class__.__name__ == "BaseParameter"
+        assert parameter.get_class_tag("variable_type") is str
+        assert parameter.get_class_tag("produces_factor") is bool
+        assert parameter.get_class_tag("is_linear_gaussian") is bool
+        assert parameter.get_class_tag("supports_fit_joint") is bool
+        assert parameter.get_class_tag("python_dependencies") is tuple
+
+    def test_fit(self):
+        X_arr, _ = make_moons(n_samples=100, noise=0.1, random_state=42)
+        X = pd.DataFrame(X_arr[:, 0].reshape(-1, 1), columns=["x"])
+        y = pd.DataFrame(X_arr[:, 1].reshape(-1, 1), columns=["y"])
+        parameter = BaseParameter()
+
+        with pytest.raises(NotImplementedError):
+            parameter.fit(X, y)
+
+    def test_predict_proba(self):
+        X_arr, _ = make_moons(n_samples=100, noise=0.1, random_state=42)
+        X = pd.DataFrame(X_arr[:, 0].reshape(-1, 1), columns=["x"])
+        parameter = BaseParameter()
+
+        with pytest.raises(NotImplementedError):
+            parameter.predict_proba(X)
