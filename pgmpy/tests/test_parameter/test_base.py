68a54c27337321a1751fa96c0870d45578d96c6d
김대현 (Kim DaeHyun) <daehyun99@kakao.com>
2026-06-26 15:54:25 +0900

    test

--- pgmpy/tests/test_parameter/test_base.py
+++ pgmpy/tests/test_parameter/test_base.py
@@ -35,3 +35,7 @@ class TestBaseParameter:
 
         with pytest.raises(NotImplementedError):
             parameter.predict_proba(X)
+
+    def test_get_parameter_estimator(self):
+        """Test `get_parameter_estimator`"""
+        ...
