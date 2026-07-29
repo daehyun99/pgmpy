8de131fd84aad79d0362dc79f79a79c065206577
daehyun99 <daehyun99@kakao.com>
2026-07-29 15:39:00 +0900

    BayesianFunctionalRegression

--- pgmpy/parameter/BayesianFunctionalRegression.py
+++ pgmpy/parameter/BayesianFunctionalRegression.py
@@ -6,16 +6,11 @@ import torch
 from pyro.infer import SVI, Predictive, Trace_ELBO
 from pyro.infer.autoguide import AutoDiagonalNormal
 from pyro.nn import PyroModule, PyroSample
-from skpro.distributions.normal import Normal as SkproNormal
 from torch import nn
 
 from pgmpy.parameter._base import BaseParameter
 
-pyro.set_rng_seed(1)
-plt.style.use("default")
-
-
-class BayesianLinearRegression(BaseParameter):
+class BayesianFunctionalRegression(BaseParameter):
     _tags = {
         "variable_type": "continuous",
         "produces_factor": False,
@@ -27,34 +22,24 @@ class BayesianLinearRegression(BaseParameter):
     }
     def __init__(
         self,
-        in_features,
+        model,
+        guide,
+        converter,
         num_iterations=1500,
         lr=0.03,
         posterior_samples=2000,
     ):
         super().__init__()
 
-        self.in_features = in_features
         self.num_iterations = num_iterations
         self.lr = lr
         self.posterior_samples = posterior_samples
 
-        self.model = None
-        self.guide = None
-        self._is_fitted = False
-    class _Model(PyroModule):
-        def __init__(self, in_features, mu=0.0, sigma=1.0):
-            super().__init__()
-            self.linear = PyroModule[nn.Linear](in_features, 1)
-            self.linear.weight = PyroSample(dist.Normal(mu, sigma).expand([1, in_features]).to_event(2))
-            self.linear.bias = PyroSample(dist.Normal(mu, sigma).expand([1]).to_event(1))
+        self.model = model if model else None
+        self.guide = guide if guide else None
+        self.converter = converter if converter else None
 
-        def forward(self, x, y=None):
-            sigma = pyro.sample("sigma", dist.Uniform(0.0, 10.0))
-            mean = self.linear(x).squeeze(-1)
-            with pyro.plate("data", x.shape[0]):
-                obs = pyro.sample("obs", dist.Normal(mean, sigma), obs=y)
-            return mean
+        self._is_fitted = False
 
     def fit(self, X, y):
         if not isinstance(X, torch.Tensor):
@@ -71,8 +56,6 @@ class BayesianLinearRegression(BaseParameter):
             X_tensor, y_tensor = X, y
 
         pyro.clear_param_store()
-        self.model = self._Model(self.in_features)
-        self.guide = AutoDiagonalNormal(self.model)
 
         optimizer = pyro.optim.Adam({"lr": self.lr})
         svi = SVI(
@@ -121,20 +104,5 @@ class BayesianLinearRegression(BaseParameter):
             samples = predictive(X_tensor)
             y_samples = samples["obs"]
 
-        y_samples = y_samples.reshape(num_samples, X_array.shape[0])
-
-        pred_mean = y_samples.mean(dim=0)
-        pred_sigma = y_samples.std(dim=0, unbiased=False)
-
-        eps = torch.finfo(pred_sigma.dtype).eps
-        pred_sigma = pred_sigma.clamp_min(eps)
-
-        pred_mean = pred_mean.detach().cpu().numpy()
-        pred_sigma = pred_sigma.detach().cpu().numpy()
-
-        return SkproNormal(
-            mu=pred_mean.reshape(-1, 1),
-            sigma=pred_sigma.reshape(-1, 1),
-            index=index,
-            columns=pd.Index(["y"]),
-        )
+        pred_dist = self.converter(y_samples, num_samples, X_array, index=index, columns=pd.Index(["y"]))
+        return pred_dist
