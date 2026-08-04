c586dca17ec7d5504155d19001b6c45dcd1e811c
daehyun99 <daehyun99@kakao.com>
2026-08-04 23:21:32 +0900

    TEST: Add test code for `PyroToSkpro`

--- pgmpy/tests/test_distributions/test_PyroToSkpro.py
+++ pgmpy/tests/test_distributions/test_PyroToSkpro.py
@@ -1,1 +1,163 @@
-class TestPyroToSkpro: ...
+import math
+
+import numpy as np
+import pandas as pd
+import pytest
+from skbase.utils.dependencies import _safe_import
+from skpro.distributions.laplace import Laplace
+from skpro.distributions.normal import Normal
+
+from pgmpy.distributions.converter.PyroToSkpro import PyroToSkpro
+
+torch = _safe_import("torch")
+pyro = _safe_import("pyro", pkg_name="pyro-ppl")
+
+
+@pytest.fixture
+def samples():
+    samples = {
+        "obs": torch.tensor(
+            [
+                [1.0, 2.0],
+                [3.0, 4.0],
+                [5.0, 6.0],
+            ]
+        )
+    }
+    return samples
+
+
+class TestPyroToSkpro:
+    def test_normal(self, samples):
+
+        converter = PyroToSkpro("normal")
+        normal = converter.convert(samples, pd.RangeIndex(0, 2), ["normal"], "obs")
+
+        assert isinstance(normal, Normal)
+        assert normal.shape == (2, 1)
+        np.testing.assert_allclose(
+            normal.mu,
+            np.array(
+                [
+                    [3.0],
+                    [4.0],
+                ],
+                dtype=np.float32,
+            ),
+        )
+        np.testing.assert_allclose(
+            normal.sigma,
+            np.array(
+                [
+                    [1.6329932],
+                    [1.6329932],
+                ],
+                dtype=np.float32,
+            ),
+            rtol=1e-6,
+            atol=1e-7,
+        )
+
+    def test_add(self, samples):
+        def _convert_laplace(samples, index, columns, name="obs"):
+            y_samples = samples[name]
+            mean = y_samples.mean(dim=0).detach().reshape(-1, 1).cpu().numpy()
+            scale = (y_samples.std(dim=0, unbiased=False) / math.sqrt(2)).detach().reshape(-1, 1).cpu().numpy()
+
+            return Laplace(
+                mu=mean,
+                scale=scale,
+                index=index,
+                columns=columns,
+            )
+
+        converter = PyroToSkpro("laplace").add("laplace", _convert_laplace)
+        laplace = converter.convert(samples, pd.RangeIndex(0, 2), ["laplace"], "obs")
+
+        assert isinstance(laplace, Laplace)
+        assert laplace.shape == (2, 1)
+        np.testing.assert_allclose(
+            laplace.mu,
+            np.array(
+                [
+                    [3.0],
+                    [4.0],
+                ],
+                dtype=np.float32,
+            ),
+        )
+        np.testing.assert_allclose(
+            laplace.scale,
+            np.array(
+                [
+                    [1.1547006],
+                    [1.1547006],
+                ],
+                dtype=np.float32,
+            ),
+            rtol=1e-6,
+            atol=1e-7,
+        )
+
+    def test_set_converter(self, samples):
+        def _convert_laplace(samples, index, columns, name="obs"):
+            y_samples = samples[name]
+            mean = y_samples.mean(dim=0).detach().reshape(-1, 1).cpu().numpy()
+            scale = (y_samples.std(dim=0, unbiased=False) / math.sqrt(2)).detach().reshape(-1, 1).cpu().numpy()
+
+            return Laplace(
+                mu=mean,
+                scale=scale,
+                index=index,
+                columns=columns,
+            )
+
+        converter = PyroToSkpro()
+        converter.add("laplace", _convert_laplace)
+        converter.set_converter("laplace")
+        laplace = converter.convert(samples, pd.RangeIndex(0, 2), ["laplace"], "obs")
+
+        assert isinstance(laplace, Laplace)
+        assert laplace.shape == (2, 1)
+        np.testing.assert_allclose(
+            laplace.mu,
+            np.array(
+                [
+                    [3.0],
+                    [4.0],
+                ],
+                dtype=np.float32,
+            ),
+        )
+        np.testing.assert_allclose(
+            laplace.scale,
+            np.array(
+                [
+                    [1.1547006],
+                    [1.1547006],
+                ],
+                dtype=np.float32,
+            ),
+            rtol=1e-6,
+            atol=1e-7,
+        )
+
+    def test_remove(self, samples):
+        def _convert_laplace(samples, index, columns, name="obs"):
+            y_samples = samples[name]
+            mean = y_samples.mean(dim=0).detach().reshape(-1, 1).cpu().numpy()
+            scale = (y_samples.std(dim=0, unbiased=False) / math.sqrt(2)).detach().reshape(-1, 1).cpu().numpy()
+
+            return Laplace(
+                mu=mean,
+                scale=scale,
+                index=index,
+                columns=columns,
+            )
+
+        converter = PyroToSkpro()
+        converter.add("laplace", _convert_laplace)
+        converter.set_converter("laplace")
+        converter.remove("laplace")
+        with pytest.raises(ValueError):
+            converter.convert(samples, pd.RangeIndex(0, 2), ["laplace"], "obs")
