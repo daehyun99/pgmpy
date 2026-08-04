2e1d98f65d31f9585fc94f63f26bf2278c0f43f3
daehyun99 <daehyun99@kakao.com>
2026-08-04 23:21:49 +0900

    ENH: Enhance `PyroToSkpro`

--- pgmpy/distributions/converter/PyroToSkpro.py
+++ pgmpy/distributions/converter/PyroToSkpro.py
@@ -15,17 +15,41 @@ def _convert_normal(samples, index, columns, name="obs"):
 
 
 class PyroToSkpro:
-    _CONVERTERS = {
+    _DEFAULT_CONVERTERS = {
         "normal": _convert_normal,
     }
 
     def __init__(self, posterior_type: str = "normal"):
         self.posterior_type = posterior_type
 
+        self._converters = self._DEFAULT_CONVERTERS
+
     def convert(self, samples, index, columns, name="obs"):
-        converter = self._CONVERTERS.get(self.posterior_type)
+        converter = self._converters.get(self.posterior_type)
 
         if converter is None:
             raise ValueError(f"{self.posterior_type} is not ...")
 
         return converter(samples, index, columns, name=name)
+
+    def add(
+        self,
+        posterior_type,
+        converter_fn,
+    ):
+        if not callable(converter_fn):
+            raise TypeError
+
+        self._converters[posterior_type] = converter_fn
+        return self
+
+    def set_converter(self, posterior_type):
+        self.posterior_type = posterior_type
+        return self
+
+    def remove(
+        self,
+        posterior_type,
+    ):
+        self._converters.pop(posterior_type)
+        return self
