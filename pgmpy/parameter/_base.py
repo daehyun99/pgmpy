97c48338369af541a55b3b20a17cc6b295f7c3b0
daehyun99 <daehyun99@kakao.com>
2026-08-21 23:10:35 +0900

    Refactor `_check_data`

--- pgmpy/parameter/_base.py
+++ pgmpy/parameter/_base.py
@@ -1,6 +1,7 @@
 import numpy as np
-import pandas as pd
 from skbase.base import BaseEstimator as _BaseEstimator
+
+from pgmpy.causal_discovery._base import BaseCausalDiscovery
 
 
 class BaseParameter(_BaseEstimator):
@@ -32,7 +33,7 @@ class BaseParameter(_BaseEstimator):
         -------
         self : reference to self
         """
-        X, y, sample_weight = self._check_data(X, y, sample_weight)
+        X, y, sample_weight = self._check_fit_data(X, y, sample_weight)
         self._fit(X, y, sample_weight)
         self._is_fitted = True
         return self
@@ -40,7 +41,7 @@ class BaseParameter(_BaseEstimator):
     def _fit(self, X, y=None, sample_weight=None):
         raise NotImplementedError
 
-    def predict_proba(self, X):
+    def predict_proba(self, X=None, n_samples=None):
         """Predict distribution over labels for data from features.
 
         Parameters
@@ -52,80 +53,38 @@ class BaseParameter(_BaseEstimator):
         -------
         y : skpro's Distribution class
         """
-        if not self._is_fitted:
-            raise RuntimeError(
-                f"This {self.__class__.__name__} instance is not fitted yet. Call 'fit' before calling 'predict_proba'."
-            )
-        X, _, _ = self._check_data(X)
+        X, _, _ = self._check_predict_proba_data(X)
         y_pred = self._predict_proba(X)
         return y_pred
 
     def _predict_proba(self, X):
         raise NotImplementedError
 
-    def sample(self, X):
-        if not self._is_fitted:
-            raise RuntimeError(
-                f"This {self.__class__.__name__} instance is not fitted yet. Call 'fit' before calling 'sample'."
-            )
-        X, _, _ = self._check_data(X)
+    def sample(self, X=None, n_samples=None):
+        X, _, _ = self._check_sample_data(X)
         samples = self._sample(X)
         return samples
 
     def _sample(self, X):
         raise NotImplementedError
 
-    def _check_data(self, X, y=None, sample_weight=None):
+    def _check_fit_data(self, X, y=None, sample_weight=None):
         """check train data with tag"""
-        # Check X
-        if isinstance(X, pd.Series):
-            X = X.to_frame()
-
-        if not isinstance(X, pd.DataFrame):
-            raise TypeError("X must be a pandas DataFrame.")
-
-        if X.shape[0] == 0:
-            raise ValueError("X must have at least one row.")
-
-        if X.shape[1] == 0:
-            raise ValueError("X must have at least one column.")
-
-        if not self.get_tag("missing"):
-            if X.isna().any().any():
-                raise ValueError(f"{self.__class__.__name__} cannot deal with missing values.")
+        transformer = BaseCausalDiscovery()
+        X = transformer._check_fit_data(X)
 
         if y is None:
-            n_samples = len(X)
+            self.evidences_ = None
+            self.variables_ = transformer.feature_names_in_
         else:
-            # Check y
-            if isinstance(y, pd.Series):
-                y = y.to_frame()
+            self.evidences_ = transformer.feature_names_in_
+            y = transformer._check_fit_data(y)
+            self.variables_ = transformer.feature_names_in_
 
-            if not isinstance(y, pd.DataFrame):
-                raise TypeError("y must be a pandas DataFrame.")
-
-            if y.shape[0] == 0:
-                raise ValueError("y must have at least one row.")
-
-            if y.shape[1] == 0:
-                raise ValueError("y must have at least one column.")
-
-            if self.get_tag("missing") is not True:
-                if y.isna().any().any():
-                    raise ValueError(f"{self.__class__.__name__} cannot deal with missing values.")
-
-            if y.shape[1] != 1:
-                raise ValueError(f"y must contain exactly one target column. Got {y.shape[1]} columns.")
-
-            if len(X) != len(y):
-                raise ValueError(f"X and y must have the same number of rows. Got len(X)={len(X)}, len(y)={len(y)}.")
-
-            if not X.index.equals(y.index):
-                raise ValueError("X and y must have the same index.")
-
-            n_samples = len(y)
+        # TODO: Implement missing data in suvervised, unsuvervised learning
 
         # Check sample_weight
+        n_samples = len(X)
         if sample_weight is None:
             sample_weight = np.ones(n_samples, dtype=float)
         else:
@@ -138,3 +97,32 @@ class BaseParameter(_BaseEstimator):
                 raise ValueError("sample_weight cannot contain negative values.")
 
         return X, y, sample_weight
+
+    def _check_predict_proba_data(self, X=None, n_samples=None):
+        if not self._is_fitted:
+            raise RuntimeError(
+                f"This {self.__class__.__name__} instance is not fitted yet. Call 'fit' before calling 'predict_proba'."
+            )
+
+    def _check_sample_data(self, X=None, n_samples=None):
+        if not self._is_fitted:
+            raise RuntimeError(
+                f"This {self.__class__.__name__} instance is not fitted yet. Call 'fit' before calling 'sample'."
+            )
+        # Supervised
+        if (X is None) or (len(X) == len(n_samples)):
+            raise ValueError
+        else:
+            if len(X) == 1:
+                # broadcast
+                ...
+
+            else:
+                ...
+
+        # Unsupervised
+        if (X is not None) or (n_samples is None):
+            raise ValueError
+        else:
+            # sampling
+            ...
